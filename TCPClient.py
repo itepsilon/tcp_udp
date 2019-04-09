28dfe0d0934d1dbfa3e0f81739775e9e6c7123f4
itepsilon <ganbold.li@gmail.com>
2019-04-09 00:58:54 -0700

    second commit

--- TCPClient.py
+++ TCPClient.py
@@ -1,15 +1,15 @@
 from socket import socket, AF_INET, SOCK_STREAM
 
-serverName = ""
+serverName = "ec2-34-221-214-226.us-west-2.compute.amazonaws.com"
 serverPort = 12000
 
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
-sentence = input("Input lowercase sentence: ")
+sentence = raw_input("Input lowercase sentence: ")
 
 clientSocket.send(sentence)
 
-modifiedSentence, serverAddress = clientSocket.recv(1024)
+modifiedSentence = clientSocket.recv(1024)
 print("I have received modified info from server: " + modifiedSentence)
 
 clientSocket.close()
--- UDPClient.py
+++ UDPClient.py
@@ -1,10 +1,10 @@
 from socket import socket, AF_INET, SOCK_DGRAM
 
-serverName = ""
+serverName = "ec2-34-221-214-226.us-west-2.compute.amazonaws.com"
 serverPort = 12001
 
 clientSocket = socket(AF_INET, SOCK_DGRAM)
-message = input("Input lowercase sentence:")
+message = raw_input("Input lowercase sentence: ")
 
 clientSocket.sendto(message, (serverName, serverPort))
 modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
